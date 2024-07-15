5e9f4d513cb5ac1a02e8923139137365da53af78
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-07-15 15:26:12 +0200

    change the font sizaé and add the icons

--- psd-project/src/components/Section4.jsx
+++ psd-project/src/components/Section4.jsx
@@ -2,29 +2,30 @@
 const Section4 = () => {
   return (  
   <>
-    <div className="bg-[#484848] text-white pt-12 pb-12 pl-10">
-      <h1 className="text-2xl">Interested in connecting? </h1>
-      <p className="text-[10px] mt-1">Let's talk projects, collaborations or anything design!</p>
-      <button className="bg-[#252729]  text-[8px]  mt-2  rounded px-3 py-1">Book a call</button>
+    <div className="bg-[#484848] text-white py-24 pl-20">
+      <h1 className="text-4xl">Interested in connecting? </h1>
+      <p className="text-sm mt-1">Let's talk projects, collaborations or anything design!</p>
+      <button className="bg-[#252729]  text-[12px]  mt-2  rounded px-3 py-1">Book a call</button>
     </div>
-    <div className="bg-[#252729] text-white divide-y divide-salate-900">
-      <div>
-        <div className="flex py-6 px-6">
-          <p className="text-[12px]">Callum Hayes</p>
-          <ul className="flex text-[8px] px-8">
-            <li className="pr-6">Projects</li>
-            <li className="pr-6">Resume</li>
-            <li>About</li>
-          </ul>
-          <ul className="flex pl-32 text-[10px]">
-            <li className="pr-6">Website</li>
-            <li className="pr-6">linkedIn</li>
-            <li className="pr-6">Be</li>
-            <li>Xing</li>
+
+    <div className="bg-[#252729] text-white divide-y divide-salate-100">
+
+      <div className="flex justify-between py-8">
+        <div className="flex px-12">
+          <p className="text-xl font-semibold">Callum Hayes</p>
+          <ul className="flex text-sm px-12 pt-1">
+            <li className="pr-10">Projects</li>
+            <li className="pr-10">Resume</li>
+            <li className="pr-64">About</li>
           </ul>
         </div>
+        <div className="pr-12" >
+        <img src="../images/Buttons.svg" alt="buttons" 
+        width="160px" />
+        </div>
       </div>
-      <div className="text-center text-[8px] py-8">Copyright 2024 by Callum Hayes</div>
+
+      <div className="text-center text-[12px] py-8">Copyright 2024 by Callum Hayes</div>
     </div>
   </>
   )
