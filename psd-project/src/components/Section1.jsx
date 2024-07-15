ebac87cff41021fe9fc6e8d6423d76feb7f88968
gulmina <gulmina44@gmail.com>
2024-07-15 13:11:23 +0200

    final version of section1 added

--- psd-project/src/components/Section1.jsx
+++ psd-project/src/components/Section1.jsx
@@ -1,31 +1,34 @@
 const Section1 = () => {
   return (
     <>
-      <div className="bg-white w-full p-10">
-        <div className="border-2">
-          <nav>
-            <ul className="grid grid-cols-10">
-              <li className="col-span-6">Callum Hayes</li>
-              <li>Projects</li>
-              <li>Resume</li>
-              <li>About me</li>
-              <li className="">
-                <button type="button">Book a Call</button>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="flex flex-col justify-items-center border border-2">
-          <div>
-            <img src="../images/smallprofilepic.svg" alt="profile-pic" />
-            <p>
+      <div className="bg-white w-full p-4">
+        <nav className="text-xl p-8">
+          <ul className="grid grid-cols-10">
+            <li className="col-span-6">Callum Hayes</li>
+            <li className="border-0 text-center p-1">Projects</li>
+            <li className="border-0 text-center p-1">Resume</li>
+            <li className="border-0 text-center p-1">About me</li>
+            <li className="border-2  rounded-md text-center p-1">
+              <button type="button">Book a Call</button>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex flex-col px-32 py-10">
+          <div className="flex flex-col gap-6 pb-8">
+            <img
+              src="../images/smallprofilepic.svg"
+              width="80px"
+              alt="profile-pic "
+            />
+            <p className="text-6xl">
               I'm Callum Hayes, a UX/UI designer specialising in creating sleek,
               user-friendly mobile apps.
             </p>
-            <img src="../images/Buttons.svg" alt="buttons" />
+            <img src="../images/Buttons.svg" alt="buttons" width="160px" />
           </div>
 
-          <div className="flex justify-items-center gap-x-4">
+          <div className="flex justify-between gap-6">
             <img src="../images/image 89.svg" alt="image89" />
             <img src="../images/image 90.svg" alt="iamges90" />
             <img src="../images/image 91.svg" alt="image 91" />
